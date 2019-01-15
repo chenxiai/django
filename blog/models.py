a5bb0e7f9657e721049c85b29478060ea348b324
Administrator <222@qq.com>
2019-01-15 12:00:27 +0800

    # 1：采用python manage.py makemigrations 生成0001_initial.py文件  # 2: 采用 python manage.py migrate 生成数据库的表  # 3：在view.py文件中查询数据库     article = Article.objects.get(id=1)     return render(request, "index.html", {'article': article})

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+# class Person(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+
+# 通过定义class来自动生成表
+class Article(models.Model):
+    # 默认会生成自动增长的主键
+    title = models.CharField(max_length=30,default='my title')
+    content = models.TextField(null=True)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Article
+
 
 # 用来接收前端请求的模块,每个请求对应一个函数
 # 所有请求信息默认都被封装到request对象中
@@ -7,4 +9,9 @@ def index(request):
     # return HttpResponse("hello django!!!")
     # 可以采用dict类型给index页面传递参数
     # 默认所有的页面都存储在templates目录中
-    return render(request, "index.html", {'hello': 'hello django'})
+    # return render(request, "index.html", {'hello': 'hello django'})
+
+    # 1：调用业务逻辑类
+    # 2：调用数据访问层，由于django采用orm映射，因此直接操作类即可
+    article = Article.objects.get(id=1)
+    return render(request, "index.html", {'article': article})
