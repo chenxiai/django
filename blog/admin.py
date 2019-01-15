e2096a4aadbe0f44f66085651ac54fff598ebf52
Administrator <222@qq.com>
2019-01-15 14:47:20 +0800

    创建了后台管理工具,查看admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content']
+
 
 # Register your models here.
+admin.site.register(Article, ArticleAdmin)
